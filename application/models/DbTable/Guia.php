4e4d72f5543d517524c7424b0ed43417b5903062
grazi <grazi@User.home>
2014-01-31 17:20:21 -0200

    Corrigido Slider

--- application/models/DbTable/Guia.php
+++ application/models/DbTable/Guia.php
@@ -4,6 +4,19 @@ class Application_Model_DbTable_Guia extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'guias';
+    
+    public function pesquisarGuia($id = null, $where = null, $order = null, $limit = null){
+        if( !is_null($id) ){
+            $arr = $this->find($id)->toArray();
+            return $arr[0];
+        }else{
+            $select = $this->select()->from($this)->order($order)->limit($limit);
+            if(!is_null($where)){
+                $select->where($where);
+            }
+            return $this->fetchAll($select)->toArray();
+        }
+    }
 
 
 }
--- application/modules/admin/controllers/GuideController.php
+++ application/modules/admin/controllers/GuideController.php
@@ -5,12 +5,24 @@ class Admin_GuideController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $usuario = Zend_Auth::getInstance()->getIdentity();
+        Zend_Layout::getMvcInstance()->assign('usuario', $usuario);
+        
+        if ( !Zend_Auth::getInstance()->hasIdentity() ) {
+                return $this->_helper->redirector->goToRoute( array('module'=>'admin','controller' => 'index'), null, true);
+        }
     }
 
     public function indexAction()
     {
-        // action body
+        $bdGuia = new Application_Model_DbTable_Guia();
+        $artigos = $bdArtigos->pesquisarArtigo();
+        
+        $paginator = Zend_Paginator::factory($artigos);
+        $paginator->setItemCountPerPage(50);
+        $paginator->setPageRange(10);
+        $paginator->setCurrentPageNumber($this->_request->getParam('pagina'));
+        $this->view->paginator = $paginator;
     }
 
 
