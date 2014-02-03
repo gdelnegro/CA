539ad51a5d94503697499e95dfaa9ae97c09c781
gustavo <gustavo@localhost.localdomain>
2014-02-03 18:07:24 -0200

    Alterado layout da exibição do artigo

--- application/modules/default/controllers/NoticiasController.php
+++ application/modules/default/controllers/NoticiasController.php
@@ -18,8 +18,10 @@ class Default_NoticiasController extends Zend_Controller_Action
     
     public function showAction(){
         $dbNoticias = new Application_Model_DbTable_Artigo();
+        $dadosNoticias = $dbNoticias->pesquisarArtigo();
         $noticias = $dbNoticias->pesquisarArtigo( $this->_getParam('id') );
         $this->view->noticia = $noticias;
+        $this->view->dadosNoticias = $dadosNoticias;
     }
 
 
